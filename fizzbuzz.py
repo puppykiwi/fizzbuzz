55f1b123ce9d9f896a8aead771a2e14660e8ad21
puppykiwi <johnraymwendwa04@gmail.com>
2023-05-21 12:13:12 +0300

    Added ability to pass a max value from the terminal

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,9 @@
+from sys import argv as args #get command line arguments
+
 def fizzbuzz(max_num = 100 ):
-    
+    if len(args) > 1:
+        max_num = int(args[1])
+        
     for i in range(1, max_num + 1):
         if i % 3 == 0 and i % 5 == 0:
             print(i,"- FizzBuzz")
